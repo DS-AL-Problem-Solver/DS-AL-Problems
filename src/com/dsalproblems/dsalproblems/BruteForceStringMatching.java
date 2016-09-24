3064f3285b1fcac303219f026186c2654d0ade99
DS-AL-Problem-Solver <punyabrata.dhar@gmail.com>
2016-09-24 13:28:55 -0500

    Added the brute force solution to a string matching problem.

---
+++ src/com/dsalproblems/dsalproblems/BruteForceStringMatching.java
@@ -0,0 +1,58 @@
+package com.dsalproblems.dsalproblems;
+
+/**
+ * This class holds the logic of the brute force string matching algorithm.
+ */
+public class BruteForceStringMatching {
+    /**
+     * This method checks if a given pattern exits in a given text by using the brute force algorithm.
+     * @param text The given text.
+     * @param pattern The given pattern.
+     * @param textLength The length of the given text.
+     * @param patternLength The length of the given pattern.
+     * @return The index of the text where the pattern starts. This will return -1 if the pattern doesn't exist in the
+     * text. -2 will be returned if there is anything wrong with the input.
+     */
+    public int findBruteForceMatch(String text, String pattern, int textLength, int patternLength) {
+        // Wrong input if the text or the pattern is null
+        if (text == null || pattern == null) {
+            return -2;
+        }
+        // Wrong input if the given length of the text (or the pattern) doesn't match with the actual length of text
+        // (or the pattern).
+        if (text.length() != textLength || pattern.length() != patternLength) {
+            return -2;
+        }
+        if (textLength < 0 || patternLength < 0) {
+            return -2;
+        }
+        if (patternLength > textLength) {
+            return -1;
+        }
+        boolean matchFound = false;
+        for (int i = 0 ; i < textLength ; ++i) {
+            int j = 0;
+            for ( ; j < patternLength ; ++j) {
+                if (!matchFound && (text.charAt(i+j) == pattern.charAt(j))) {
+                    matchFound = true;
+                    j++;
+                }
+                if (matchFound) {
+                    if (textLength - i < patternLength) {
+                        return -1;
+                    }
+                    if (!(text.charAt(i+j) == pattern.charAt(j))) {
+                        matchFound = false;
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (j == patternLength) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
--- src/com/dsalproblems/main/Main.java
+++ src/com/dsalproblems/main/Main.java
@@ -1,6 +1,7 @@
 package com.dsalproblems.main;
 
 import com.dsalproblems.dsalproblems.ArrayReverse;
+import com.dsalproblems.dsalproblems.BruteForceStringMatching;
 
 /**
  * The main class.
@@ -10,7 +11,45 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello!");
+//        testArrayReverse();
+        testBruteForceStringMatching();
+    }
+
+    /**
+     * This method tests the array reverse logic.
+     */
+    private static void testArrayReverse() {
         ArrayReverse arrayReverse = new ArrayReverse();
         arrayReverse.reverseArray();
+    }
+
+    /**
+     * This method tests the brute force string matching algorithm.
+     */
+    private static void testBruteForceStringMatching() {
+        int result;
+        BruteForceStringMatching bruteForceStringMatching = new BruteForceStringMatching();
+        result = bruteForceStringMatching.findBruteForceMatch("ababaca", "aca", 7, 3);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch(null, null, 0, 0);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", null, 0, 0);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch(null, "somepattern", 0, 0);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", "somepattern", 0, 0);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", "somepattern", 8, 0);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", "somepattern", 0, 11);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", "somepattern", 8, 11);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", "pattern", 8, 7);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", "some", 8, 4);
+        System.out.println("Result: " + result);
+        result = bruteForceStringMatching.findBruteForceMatch("sometext", "texting", 8, 7);
+        System.out.println("Result: " + result);
     }
 }
